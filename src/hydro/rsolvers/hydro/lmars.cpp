1bced089a1d0e902f019e749a81277441ba44b01
luminoctum <lumos.leee@gmail.com>
2018-03-15 09:55:03 -0700

    correct lmars, including latent heat

--- src/hydro/rsolvers/hydro/lmars.cpp
+++ src/hydro/rsolvers/hydro/lmars.cpp
@@ -7,7 +7,7 @@
 #include "../../../athena_arrays.hpp"
 #include "../../../microphysics/microphysics.hpp"
 #include "../../../eos/eos.hpp"
-//#include "../../../math_funcs.hpp"
+#include "../../../math_funcs.hpp"
 
 void Hydro::RiemannSolver(int const k, int const j, int const il, int const iu,
     int const ivx, AthenaArray<Real> const& bx, AthenaArray<Real> &wl,
@@ -24,8 +24,9 @@ void Hydro::RiemannSolver(int const k, int const j, int const il, int const iu,
   for (int i = il; i <= iu; ++i) {
     // correction for gamma
     // left
-    Real fsig = 1., feps = 1.;
+    Real fsig = 1., feps = 1., lel = 0., ler = 0.;
     for (int n = ICD; n < ICD + NVAPOR; ++n) {
+      lel += -pmicro->GetLatent(n)*wl(n,i);
       fsig += wl(n,i)*(pmicro->GetCvRatio(n) - 1.);
       feps -= wl(n,i);
     }
@@ -33,11 +34,12 @@ void Hydro::RiemannSolver(int const k, int const j, int const il, int const iu,
       fsig += wl(n,i)*(pmicro->GetCvRatio(n) - 1.);
       feps += wl(n,i)*(1./pmicro->GetMassRatio(n) - 1.);
     }
-    Real kappal = 1./(gamma - 1.) + feps/fsig;
+    Real kappal = 1./(gamma - 1.)*fsig/feps;
 
     // right
     fsig = 1., feps = 1.;
     for (int n = ICD; n < ICD + NVAPOR; ++n) {
+      ler += -pmicro->GetLatent(n)*wr(n,i);
       fsig += wr(n,i)*(pmicro->GetCvRatio(n) - 1.);
       feps -= wr(n,i);
     }
@@ -45,11 +47,11 @@ void Hydro::RiemannSolver(int const k, int const j, int const il, int const iu,
       fsig += wr(n,i)*(pmicro->GetCvRatio(n) - 1.);
       feps += wr(n,i)*(1./pmicro->GetMassRatio(n) - 1.);
     }
-    Real kappar = 1./(gamma - 1.) + feps/fsig;
+    Real kappar = 1./(gamma - 1.)*fsig/feps;
 
     rhobar = 0.5*(wl(IDN,i) + wr(IDN,i));
-    hl = wl(IPR,i)/wl(IDN,i)*kappal + 0.5*(wl(ivx,i)*wl(ivx,i) + wl(ivy,i)*wl(ivy,i) + wl(ivz,i)*wl(ivz,i));
-    hr = wr(IPR,i)/wr(IDN,i)*kappar + 0.5*(wr(ivx,i)*wr(ivx,i) + wr(ivy,i)*wr(ivy,i) + wr(ivz,i)*wr(ivz,i));
+    hl = wl(IPR,i)/wl(IDN,i)*(kappal + 1.) + 0.5*(_sqr(wl(ivx,i)) + _sqr(wl(ivy,i)) + _sqr(wl(ivz,i))) + lel;
+    hr = wr(IPR,i)/wr(IDN,i)*(kappar + 1.) + 0.5*(_sqr(wr(ivx,i)) + _sqr(wr(ivy,i)) + _sqr(wr(ivz,i))) + ler;
     cbar = sqrt(0.5*gamma*(wl(IPR,i)+ wr(IPR,i))/rhobar);
     pbar = 0.5*(wl(IPR,i) + wr(IPR,i)) + 0.5*(rhobar*cbar)*(wl(ivx,i) - wr(ivx,i));
     ubar = 0.5*(wl(ivx,i) + wr(ivx,i)) + 0.5/(rhobar*cbar)*(wl(IPR,i) - wr(IPR,i));
